380ce88f5df8bd8cf5295838c77806ab070cbd65
Anish <anishupreti2002@gmail.com>
2025-02-20 10:20:53 +0545

    code updated

--- Easy Leetcodes/392_Is_Subsequence.py
+++ Easy Leetcodes/392_Is_Subsequence.py
@@ -1,24 +1,5 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-
-        # # one way to solve
-        # s1 = list(s)
-        # t1 = list(t)
-        # r = []
-
-        # for i in s:
-        #     if i in t1:
-        #         r.append(i)
-        #         t1.remove(i)
-        # r1 ="".join(r)
-        # if r1==s:
-        #     return True
-        # else:
-        #     return False
-
-        # ***********************************************   
-
-        # Another way to solve
         
         if len(s) == 0: return True
         if len(s) > len(t): return False
