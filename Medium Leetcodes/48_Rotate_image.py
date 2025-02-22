4d0825cb13fb0d585c7c73805e49326f68133871
Anish <anishupreti2002@gmail.com>
2025-02-22 21:18:40 +0545

    first commit

---
+++ Medium Leetcodes/48_Rotate_image.py
@@ -0,0 +1,45 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        n = len(matrix)
+        #vertical reversal
+        top = 0
+        bottom = n - 1
+
+        while top < bottom:
+            for i in range(n):
+                temp = matrix[top][i]
+                matrix[top][i] = matrix[bottom][i]
+                matrix[bottom][i] = temp
+            top += 1
+            bottom -= 1
+
+        # Transpose
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        return matrix
+
+
+# Alternate code
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        
+        # Tranpose
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # Reflection
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
