4ea4ed1523c9225c028e1ce13f12618304f102c0
Anish <anishupreti2002@gmail.com>
2025-02-25 22:32:40 +0545

    first commit

---
+++ Easy Leetcodes/66_Plus_one.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # loop through the list from last to first element
+        for i in range(len(digits)-1, -1, -1):
+            # if current digit is 9, make it 0
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                # if the digit is less than 9, increment it by 1 and return the list
+                digits[i] = digits[i] + 1
+                return digits
+        # if all the elements are 9, then insert 1 at the beginning of the list
+        return [1] + digits
