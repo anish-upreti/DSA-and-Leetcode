2b75657780bf9c198ff72cbcf57b6cb28edbc880
Anish <anishupreti2002@gmail.com>
2025-02-27 10:09:32 +0545

    first commit

---
+++ Easy Leetcodes/70_Climbing_stairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        prev1 = 2
+        prev2 = 1
+        curr = 0
+
+        for i in range(2,n):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+
+        return curr
+    
+
+# Alternate solution
+
+       
+
